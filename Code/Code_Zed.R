8fb3177f2b5381c4f2049e7c36c292dfad32eb51
Zeyu Li <zli995@wisc.edu>
2020-11-24 14:36:10 -0600

    update zed's part

--- Code/Code_Zed.R
+++ Code/Code_Zed.R
@@ -1,36 +1,35 @@
-## read Chinese restaurants review file and load packages
+######## Step1: Loading Data and Select Key Words ########
 
+#a.read Chinese restaurants review file and load packages
 library(tidytext); library(dplyr); library(wordcloud2); library(ggplot2); library(magrittr)
 ch_review = read.csv("chinese_review.csv", stringsAsFactors = F)
 
-## Targeted Words and Associated Adjective
-meat = c("beef", "shrimp", "pork", "chicken", "crab", "duck", "lamb", "lobster")
-#Question: How to find the undesirable words?
+#b.Targeted Words and Associated Adjective
+meat = c("beef", "shrimp", "pork", "chicken", "crab", "duck", "lamb", "lobster") #Question: How to find the undesirable words?
 
 
-## Tokenization and Getting Rid of The Stop Words
+#c.Tokenization and Getting Rid of The Stop Words
 Ch_filtered <- ch_review %>%
   unnest_tokens(word,text) %>%
   anti_join(stop_words) %>%
   distinct() %>%
-  filter(nchar(word) > 3)
-#Notice: Don't forget to add the undesirable words
+  filter(nchar(word) > 3) #Notice: Don't forget to add the undesirable words
 
-
-## Select Distinct Targeted Words
+#d.Select Distinct Targeted Words
 target <- function(x){
-    Ch_filtered %>% 
-    filter(word == x) %>%
-    select(business_id,stars,category,word) %>%
-    arrange() %>%
-    distinct(business_id,.keep_all = TRUE)
+  Ch_filtered %>% 
+  filter(word == x) %>%
+  select(business_id,stars,category,word) %>%
+  arrange() %>%
+  distinct(business_id,.keep_all = TRUE)
 }
+
 for(i in meat){
   nam = paste(i, sep = " ")
   assign(nam, target(i))
 }
 
-## Category Plot
+######## Step 2: Analyzing Under Big Domain ######## 
 complete_mentioned = rbind(beef,crab,chicken,duck,frog,lamb,lobster,pork,shrimp)
 mentioned_times = count(complete)
 times_meat <- data.frame(count=c(556, 712, 499, 209, 24, 85, 135, 521, 575),
